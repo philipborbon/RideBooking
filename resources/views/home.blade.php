1d673484396d159e0713307311ca82c73b04b248
Philip Borbon <philipjohnborbon@gmail.com>
2019-05-22 08:20:58 +0800

    Home and breadcrumbs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,97 @@
-@extends('layouts.app')
+@extends('layouts.home')
 
 @section('content')
+
 <div class="container">
-    <div class="row mt-5">
-        <div class="col-md-8 offset-md-2">
-            <div class="card">
+    <div class="row mt-3">
+        <div class="col-md-12">
+<!--             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
+                <div class="card-body"> -->
+                    <h1>Dashboard</h1>
+
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    You are logged in!
-                </div>
+                    <div class="row mt-3">
+                        <div class="col-xs-6 col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                    <a href="/users" class="btn btn-primary btn-lg btn-block" role="button">Users</a>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col">
+                                    <a href="/wallets" class="btn btn-success btn-lg btn-block" role="button">Wallets</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                <a href="/vehicles" class="btn btn-success btn-lg btn-block" role="button">Vehicles</a>
+                                </div>
+                                <div class="col-md-6">
+                                <a href="/routes" class="btn btn-success btn-lg btn-block" role="button">Routes</a>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col">
+                                    <a href="/passengertypes" class="btn btn-primary btn-lg btn-block" role="button">Passenger Types</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-xs-6 col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                <a href="/topups" class="btn btn-primary btn-lg btn-block" role="button">Topups</a>
+                                </div>
+                                <div class="col-md-6">
+                                <a href="/redeems" class="btn btn-primary btn-lg btn-block" role="button">Redeems</a>
+                                </div>
+                            </div>
+
+                            <div class="row mt-2">
+                                <div class="col">
+                                <a href="/collections" class="btn btn-success btn-lg btn-block" role="button">Collections</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xs-6 col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                <a href="/rideschedules" class="btn btn-success btn-lg btn-block" role="button">Schedules</a>
+                                </div>
+                                <div class="col-md-6">
+                                <a href="/bookings" class="btn btn-success btn-lg btn-block" role="button">Bookings</a>
+                                </div>
+                            </div>
+
+                            <div class="row mt-2">
+                                <div class="col">
+                                <a href="/transactions" class="btn btn-primary btn-lg btn-block" role="button">Transactions</a>
+                                </div>
+                            </div>
+
+                            <div class="row mt-2">
+                                <div class="col">
+                                <a href="/reports" class="btn btn-primary btn-lg btn-block" role="button">Reports</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+<!--                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 @endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -54,24 +54,7 @@
 
         </div>
     </nav>
-
-    <div class="container mt-3 mb-3">
-        <a href="/">Home</a> >
-        <?php $link = "" ?>
-        @for($i = 1; $i <= count(Request::segments()); $i++)
-            @if($i < count(Request::segments()) & $i > 0)
-
-            <?php $link .= "/" . Request::segment($i); ?>
-            <?php 
-                $text = ucwords(str_replace('-',' ',Request::segment($i)));
-                if (is_numeric($text)) { continue; }
-            ?>
-            <a href="<?= $link ?>">{{ $text }}</a> >
-            @else {{ucwords(str_replace('-',' ',Request::segment($i)))}}
-            @endif
-        @endfor
-    </div>
-
+    
     @yield('content')
 </div>
 
